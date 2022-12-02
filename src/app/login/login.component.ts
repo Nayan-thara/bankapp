1fba6cd2c1648b10b1a61d6292b891f8db1e960f
NaYaN ThArA <nayannivu9gmail.com>
2022-12-02 14:17:25 +0530

    initial commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,8 +10,8 @@ export class LoginComponent {
   
   //  data="Account no" //property binding
 
-  // acno="" //event binding using $event
-  // pw=""
+  acno="" //event binding using $event
+  psw=""
 
   userDetails:any={
     1000:{acno:1000,username:"nivya",password:123,balance:0},
@@ -21,19 +21,56 @@ export class LoginComponent {
   }
 
 
-  login(){ //event binding
-    alert('login clicked')
-
+  //rendering method  
+  login(a:any,b:any){
+    
+    // console.log(a.value); // a elemnt an so athile value kittan .value use cheyanam
+    
+  this.acno=a.value
+  this.psw=b.value
+  
+    var acno=this.acno
+    var psw=this.psw
+    var userDetails=this.userDetails
+    if(acno in userDetails){
+       if(psw==userDetails[acno]["password"]){
+        alert("login success")
+       }
+       else{
+        alert('incurrect password')
+       }
+    }
+    else{
+      alert('incorrect username')
+    }
+   
   }
 
 
-  //event binding using $event
-  // acnoChange(event:any){
-  //   this.acno=event.target.value
-
+//event binding //dolar event
+  // login(){
+  //   var acno=this.acno
+  //   var psw=this.psw
+  //   var userDetails=this.userDetails
+  //   if(acno in userDetails){
+  //      if(psw==userDetails[acno]["password"]){
+  //       alert("login success")
+  //      }
+  //      else{
+  //       alert('incurrect password')
+  //      }
+  //   }
+  //   else{
+  //     alert('incorrect username')
+  //   }
+   
   // }
-  // pwChange(event:any){
-  //   this.pw=event.target.value
+  //$event
+  // acnoChange(event:any){
+  //   this.acno=event.target.value}
+
+  // pswChange(event:any){
+  //   this.psw=event.target.value
   // }
 
 
